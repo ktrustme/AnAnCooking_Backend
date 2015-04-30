279cae610eb1aa71934c002f7c8c6737c03d1195
Xin <kuoxinkuoxin@gmail.com>
2015-04-30 15:04:38 -0400

    add several servlet

--- src/com/anan/anancooking/server/database/AbstractDBProxy.java
+++ src/com/anan/anancooking/server/database/AbstractDBProxy.java
@@ -4,247 +4,256 @@ package com.anan.anancooking.server.database;
  * Created by zhouyangdi on 4/29/15.
  */
 
-    import com.anan.anancooking.model.RecipeImplementation;
-    import com.anan.anancooking.model.RecipePreviewImplementation;
-    import com.anan.anancooking.model.Step;
+import com.anan.anancooking.model.RecipeImplementation;
+import com.anan.anancooking.model.RecipePreviewImplementation;
+import com.anan.anancooking.model.Step;
 
-    import java.io.FileOutputStream;
-    import java.io.OutputStream;
-    import java.sql.*;
-    import java.util.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.*;
+import java.util.*;
+
+import javax.imageio.ImageIO;
 
 public abstract class AbstractDBProxy {
-    // JDBC driver name and database URL
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost/";
-    private static final String USER = "username";
-    private static final String PASS = "password";
-    private static final int MAX_IMAGE = 16 * 1024;
+	// JDBC driver name and database URL
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost/";
+	private static final String USER = "username";
+	private static final String PASS = "password";
+	private static final int MAX_IMAGE = 16 * 1024;
 
-    private static final String DATABASE = "test";
-    Connection conn = null;
-    Statement stmt = null;
+	private static final String DATABASE = "test";
+	Connection conn = null;
+	Statement stmt = null;
 
-    public void addRecipe(RecipeImplementation recipe) {
+	public void addRecipe(RecipeImplementation recipe) {
 		/* get the recipe id */
-        String recipeID = RecipeIDGenerater.createID();
+		String recipeID = RecipeIDGenerater.createID();
 
-        try{
-            Class.forName(JDBC_DRIVER);
+		try{
+			Class.forName(JDBC_DRIVER);
 
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            stmt = conn.createStatement();
+			stmt = conn.createStatement();
 
-            // use databases;
-            String sql = "use " + DATABASE;
-            stmt.executeQuery(sql);
+			// use databases;
+			String sql = "use " + DATABASE;
+			stmt.executeQuery(sql);
 
 
 			/* add to recipe_overview table */
 
-            byte[] bytes = new byte[MAX_IMAGE];
-            String query = ("INSERT INTO recipe_overview VALUES(?,?,?,?,?)");
-            PreparedStatement pstmt = conn.prepareStatement(query);
+			byte[] bytes = new byte[MAX_IMAGE];
+			String query = ("INSERT INTO recipe_overview VALUES(?,?,?,?,?)");
+			PreparedStatement pstmt = conn.prepareStatement(query);
 
-            pstmt.setString(1, recipeID);
-            pstmt.setString(2, recipe.getDescription());
-            pstmt.setInt(3, recipe.getTime());
-            pstmt.setString(4, recipe.getCook());
-            // Method used to insert a stream of bytes
-            pstmt.setBytes(5, bytes);
-            pstmt.executeUpdate();
+			pstmt.setString(1, recipeID);
+			pstmt.setString(2, recipe.getDescription());
+			pstmt.setInt(3, recipe.getTime());
+			pstmt.setString(4, recipe.getCook());
+			// Method used to insert a stream of bytes
+			pstmt.setBytes(5, bytes);
+			pstmt.executeUpdate();
 
 
 
 			/* add to step table */
-            for (int i = 0; i < recipe.getSteps().size(); i ++) {
-                Step step = recipe.getSteps().get(i);
-                String query2 = ("INSERT INTO step VALUES(?,?,?,?)");
-                PreparedStatement pstmt2 = conn.prepareStatement(query2);
-                pstmt2.setString(1, recipeID);
-                pstmt2.setInt(2, i);
-                pstmt2.setString(3, step.getDes());
-                pstmt2.setBytes(4, step.getBytes());
-                pstmt2.executeUpdate(query2);
-            }
+			for (int i = 0; i < recipe.getSteps().size(); i ++) {
+				Step step = recipe.getSteps().get(i);
+				String query2 = ("INSERT INTO step VALUES(?,?,?,?)");
+				PreparedStatement pstmt2 = conn.prepareStatement(query2);
+				pstmt2.setString(1, recipeID);
+				pstmt2.setInt(2, i);
+				pstmt2.setString(3, step.getDes());
+				pstmt2.setBytes(4, step.getBytes());
+				pstmt2.executeUpdate(query2);
+			}
 
 			/* add to search table */
-            for (int i = 0; i < recipe.getIngredients().size(); i ++) {
-//				String query3 = ("INSERT INTO search VALUE(?,?)");
-//				PreparedStatement pstmt3 = conn.prepareStatement(query3);
-//				pstmt3.setString(1, recipeID);
-//				pstmt3.setString(2, recipe.getIngredients().get(i));
-//				pstmt3.executeUpdate(query3);
+			for (int i = 0; i < recipe.getIngredients().size(); i ++) {
+				//				String query3 = ("INSERT INTO search VALUE(?,?)");
+				//				PreparedStatement pstmt3 = conn.prepareStatement(query3);
+				//				pstmt3.setString(1, recipeID);
+				//				pstmt3.setString(2, recipe.getIngredients().get(i));
+				//				pstmt3.executeUpdate(query3);
 
-                String query3 = ("INSERT INTO search VALUE(\"" + recipeID + "\", \"" + recipe.getIngredients().get(i) + "\")");
-                stmt.executeUpdate(query3);
-            }
+				String query3 = ("INSERT INTO search VALUE(\"" + recipeID + "\", \"" + recipe.getIngredients().get(i) + "\")");
+				stmt.executeUpdate(query3);
+			}
 
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 
-        finally{
-            try{
-                if(stmt != null)
-                    stmt.close();
-            }
-            catch(SQLException se2){
-            }
-            try{
-                if(conn != null)
-                    conn.close();
-            }
-            catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
+		finally{
+			try{
+				if(stmt != null)
+					stmt.close();
+			}
+			catch(SQLException se2){
+			}
+			try{
+				if(conn != null)
+					conn.close();
+			}
+			catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
 
-    }
+	}
 
-    public List<RecipePreviewImplementation> searchForPreview(List<String> ingredients) {
-        List<RecipePreviewImplementation> previews = new ArrayList<RecipePreviewImplementation>();
-        StringBuilder sb = new StringBuilder("'");
-        for (int i = 0; i < ingredients.size(); i ++) {
-            sb.append(ingredients.get(i));
-            if (i == ingredients.size() - 1)
-                sb.append("'");
-            else
-                sb.append("' OR '");
-        }
-        try{
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
+	public List<RecipePreviewImplementation> searchForPreview(List<String> ingredients) {
+		List<RecipePreviewImplementation> previews = new ArrayList<RecipePreviewImplementation>();
+		StringBuilder sb = new StringBuilder("'");
+		for (int i = 0; i < ingredients.size(); i ++) {
+			sb.append(ingredients.get(i));
+			if (i == ingredients.size() - 1)
+				sb.append("'");
+			else
+				sb.append("' OR '");
+		}
+		try{
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
 
-            // use databases;
-            String sql = "use " + DATABASE;
-            stmt.executeQuery(sql);
+			// use databases;
+			String sql = "use " + DATABASE;
+			stmt.executeQuery(sql);
 
-            String sql2 = "SELECT * FROM recipe_overview WHERE recipe_id IN (SELECT recipe_id FROM search WHERE ingredient = "
-                    + sb.toString() + ")";
-            ResultSet rs = stmt.executeQuery(sql2);
-            System.out.println("fetching image");
+			String sql2 = "SELECT * FROM recipe_overview WHERE recipe_id IN (SELECT recipe_id FROM search WHERE ingredient = "
+					+ sb.toString() + ")";
+			ResultSet rs = stmt.executeQuery(sql2);
+			System.out.println("fetching image");
 			/* fetch image */
-            byte[] fileBytes;
+			byte[] fileBytes;
 
-            while (rs.next())
-            {
-                Class.forName(JDBC_DRIVER);
-                Connection conn2 = DriverManager.getConnection(DB_URL, USER, PASS);;
-                Statement stmt2 = conn2.createStatement();
+			while (rs.next())
+			{
+				Class.forName(JDBC_DRIVER);
+				Connection conn2 = DriverManager.getConnection(DB_URL, USER, PASS);;
+				Statement stmt2 = conn2.createStatement();
 
-                stmt2.executeQuery(sql);
+				stmt2.executeQuery(sql);
 
-                String recipeID = rs.getString(1);
-                String recipeName = rs.getString(2);
-                int time = rs.getInt(3);
-                String cook = rs.getString(4);
-                fileBytes = rs.getBytes(5);
+				String recipeID = rs.getString(1);
+				String recipeName = rs.getString(2);
+				int time = rs.getInt(3);
+				String cook = rs.getString(4);
+				fileBytes = rs.getBytes(5);
 
 
 
 				/* fetch all the ingredients */
-                String sql3 = "SELECT ingredient FROM search WHERE recipe_id = '" + recipeID + "'";
-                ResultSet rs2 = stmt2.executeQuery(sql3);
-                StringBuilder in = new StringBuilder();
+				String sql3 = "SELECT ingredient FROM search WHERE recipe_id = '" + recipeID + "'";
+				ResultSet rs2 = stmt2.executeQuery(sql3);
+				StringBuilder in = new StringBuilder();
 
-                while (rs2.next()) {
-                    in.append(rs2.getString(1));
-                    System.out.println(rs2.getString(1));
-                    in.append(", ");
-                }
-                System.out.println("ingredients = " + in.toString());
+				while (rs2.next()) {
+					in.append(rs2.getString(1));
+					System.out.println(rs2.getString(1));
+					in.append(", ");
+				}
+				System.out.println("ingredients = " + in.toString());
 
-                RecipePreviewImplementation pre = new RecipePreviewImplementation(recipeID,
-                        recipeName, time, cook, in.toString(), fileBytes);
+				RecipePreviewImplementation pre = new RecipePreviewImplementation(recipeID,
+						recipeName, time, cook, in.toString(), fileBytes);
 
-                previews.add(pre);
-                //				OutputStream targetFile =
-                //						new FileOutputStream(
-                //								"/Users/zhouyangdi/Desktop/oppalovesme.jpg");
-                //				targetFile.write(fileBytes);
-                //				targetFile.close();
-            }
-
-
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-
-        finally{
-            try{
-                if(stmt != null)
-                    stmt.close();
-            }
-            catch(SQLException se2){
-            }
-            try{
-                if(conn != null)
-                    conn.close();
-            }
-            catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        return previews;
-
-    }
-
-    public List<Step> getSteps(String recipeID) {
-        List<Step> steps = new ArrayList<Step>();
-
-        try{
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
-
-            // use databases;
-            String sql = "use " + DATABASE;
-            stmt.executeQuery(sql);
-
-            String sql2 = "SELECT * FROM step WHERE recipe_id = '" + recipeID + "'";
-            ResultSet rs = stmt.executeQuery(sql2);
-            byte[] fileBytes;
-
-            while (rs.next())
-            {
-                //				fileBytes = rs.getBytes(1);
-                Step step = new Step(rs.getString(1), rs.getInt(2), rs.getString(3), rs.getBytes(4));
-                steps.add(step);
-                //				OutputStream targetFile =
-                //						new FileOutputStream(
-                //								"/Users/zhouyangdi/Desktop/oppalovesme.jpg");
-                //				targetFile.write(fileBytes);
-                //				targetFile.close();
-            }
+				previews.add(pre);
+				//				OutputStream targetFile =
+				//						new FileOutputStream(
+				//								"/Users/zhouyangdi/Desktop/oppalovesme.jpg");
+				//				targetFile.write(fileBytes);
+				//				targetFile.close();
+			}
 
 
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 
-        finally{
-            try{
-                if(stmt != null)
-                    stmt.close();
-            }
-            catch(SQLException se2){
-            }
-            try{
-                if(conn != null)
-                    conn.close();
-            }
-            catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        return steps;
-    }
+		finally{
+			try{
+				if(stmt != null)
+					stmt.close();
+			}
+			catch(SQLException se2){
+			}
+			try{
+				if(conn != null)
+					conn.close();
+			}
+			catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
+		return previews;
+
+	}
+
+	public List<Step> getSteps(String recipeID) {
+		List<Step> steps = new ArrayList<Step>();
+
+		try{
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+
+			// use databases;
+			String sql = "use " + DATABASE;
+			stmt.executeQuery(sql);
+
+			String sql2 = "SELECT * FROM step WHERE recipe_id = '" + recipeID + "'";
+			ResultSet rs = stmt.executeQuery(sql2);
+			byte[] fileBytes;
+
+			while (rs.next())
+			{
+				//				fileBytes = rs.getBytes(1);
+				Step step = new Step(rs.getString(1), rs.getInt(2), rs.getString(3), rs.getBytes(4));
+				steps.add(step);
+				//				OutputStream targetFile =
+				//						new FileOutputStream(
+				//								"/Users/zhouyangdi/Desktop/oppalovesme.jpg");
+				//				targetFile.write(fileBytes);
+				//				targetFile.close();
+			}
+
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+		finally{
+			try{
+				if(stmt != null)
+					stmt.close();
+			}
+			catch(SQLException se2){
+			}
+			try{
+				if(conn != null)
+					conn.close();
+			}
+			catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
+		return steps;
+	}
+
+
 
 
 }
---
+++ src/com/anan/anancooking/server/servlet/FetchRecipeServlet.java
@@ -0,0 +1,96 @@
+/*------------------------------
+ This file: showOptionServlet.java
+ Programmer: Xin Kuo
+ Andrew ID: xkuo
+ Course/Section: 18641
+ Assignment: proj1_unit5
+ Description:	
+ Last Modified: 02/28/2015
+ Known Bugs: Currently none
+ Compiler: Java SE 7, javac
+ ------------------------------*/
+
+package com.anan.anancooking.server.servlet;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.anan.anancooking.model.RecipeImplementation;
+import com.google.gson.Gson;
+
+
+
+/**
+ * @author kuoxin
+ * This servlet is responsible for fetch all configuration info from the server
+ * and present the data to the corresponding JSP.
+ *
+ */
+public class FetchRecipeServlet extends HttpServlet {
+	//String target = "showOption.jsp";
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+
+		System.out.println("FetchRecipeRequest from app received");
+
+		String id = request.getParameter("id");
+		System.out.println("The recipe id is: "+id);
+
+		PrintWriter out = response.getWriter();
+		String title = "Auto Refresh Header Setting";
+
+		//out.println("OK...Now I think it works, and the id is: "+id);
+		//out.println("{ key1: 'value1', key2: 'value2' }");
+
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		Gson gson = new Gson();
+		//create Json Object
+		List<String> ingredients = new ArrayList<String>();
+		ingredients.add("Cabbage");ingredients.add("Pork");
+		String imageName="/image.jpg";
+		System.out.println(gson.toJson((new RecipeImplementation()).setIngredients(ingredients)
+				.setTime(5)
+				.setDescription("A strange dish").setPreviewByteCode(extractBytes(imageName))));
+		out.print(gson.toJson((new RecipeImplementation()).setIngredients(ingredients)
+				.setTime(5)
+				.setDescription("A strange dish").setPreviewByteCode(extractBytes(imageName))));
+	}
+
+
+	public byte[] extractBytes (String imageName){
+		// open image		
+		BufferedImage bufferedImage;
+		InputStream input = getServletContext().getResourceAsStream(imageName);
+
+		byte[] imageInByte=null;
+		try {
+			bufferedImage = ImageIO.read(input);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write( bufferedImage, "jpg", baos );
+			baos.flush();
+			imageInByte = baos.toByteArray();
+			baos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return imageInByte;
+	}
+}
