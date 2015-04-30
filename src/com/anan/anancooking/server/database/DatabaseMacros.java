32644bfd86eeba3c3e76c982066bdd221ad34b27
yangdi zhou <z11y21d@gmail.com>
2015-04-30 14:58:28 -0400

    database update

---
+++ src/com/anan/anancooking/server/database/DatabaseMacros.java
@@ -0,0 +1,17 @@
+package com.anan.anancooking.server.database;
+
+public class DatabaseMacros {
+	// JDBC driver name and database URL
+	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost/";
+	public static final String USER = "username";
+	public static final String PASS = "password";
+	public static final int MAX_IMAGE = 16 * 1024;
+
+	public static final String DATABASE = "test";
+	public static final String SEARCH = "search";
+	public static final String RECIPE_OVERVIEW = "recipe_overview";
+	public static final String STEP = "step";
+	
+	public static final int RECOMMEND_NO = 5;
+}
